a732731c394274a957ecd2354eb5ed9b2f7e6397
salazarchen <rweic@uw.edu>
2022-05-22 12:51:55 -0700

    fix the img intensity generation

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # EE526_Project
 
-Makefile for simulation: test/result
+### Image Convert
+- figures/cropimg.py: cut to (480, 640), turn to grayscale
+- figures/convert_binary.py: write the binary values into txt
+- figures/bin2jpg.py: regenerate the figure, from the one got from convert_binary.py
+- figures/jpg2intensity.py: write the intensity into txt file
+- 
 
-make compile
+### Makefile
+- Makefile for simulation: test/result
+- make compile
+- make simulate: generate vcd file
+- make display
+- make clean
 
-make simulate: generate vcd file
 
-make display
 
-make clean
+
 
---
+++ figures/jpg2intensity.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import numpy as np
+import re
+
+name = 'img1'
+filename = 'jpeg/' + name + '.jpg'
+
+img = Image.open(filename)
+#img = img.convert('RGB')
+img = np.array(img)
+
+print(img.size)
+print(img.shape)
+#print(img)
+
+str = ''
+for i in range(480):
+    for j in range(640):
+        hexVal = hex(img[i][j])
+        if(len(hexVal) == 3):
+            hexVal = '0x0' + hexVal[2]
+        str += hexVal + '\n'
+
+str = re.sub('0x', '', str) # Replace '0x' with 'x' for your needs
+
+save_dir = 'bf/' + name + '.txt'
+save_file = open(save_dir, 'w')
+save_file.write(str)
+save_file.close()
+
+
